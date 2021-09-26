0d60cb89cce8df14555cd1c6cff69c14af63b533
neulii <neulii@gmx.at>
2021-09-26 11:04:21 +0200

    cleard files

--- functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-void fillArray(int array[],int sizeOfArray);
-void fillArray(int array[], int sizeOfArray, int value);
-
-void printArray(int array[],int sizeOfArray);
-
-#endif
