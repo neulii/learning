2cdc4b3e92fab7697899685d12d2c5d792df1152
neulii <neulii@gmx.at>
2021-09-28 16:18:01 +0200

    now with files and changed git commit filej

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
 #include "../include/functions.hpp"
 
-
+#include <fstream>
 
 int main()
 {
-	int binaryInteger = 1;
+	using namespace std;
 
-	binaryInteger = binaryInteger << 4;
 	
-	std::cout << binaryInteger << std::endl;
+	std::ofstream file;
 
+	file.open("file.txt",std::ios::app);
+	/*
+	file << "hallo" << std::endl;
+	file << "nur ein test" << std::endl;
 
+	file.close();
+	*/
+
+	std::ifstream readFile;
+
+	readFile.open("file.txt");
+	
+	if(readFile.is_open())
+	{
+		string line;
+		string fileText;
+
+		while(getline(readFile, line))
+		{
+			fileText = fileText + line + '\n';
+
+		}
+
+			cout << fileText << endl;
+
+	}
 
 	
 }
