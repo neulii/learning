9f07f91e903f6608293ac4872383633a50ea2ab2
neulii <neulii@gmx.at>
2021-10-03 09:45:47 +0200

    jetzt funktioniert cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,16 @@
-#include <iostream>
-#include "../include/functions.hpp"
-#include "../include/material.hpp"
-#include <ncurses.h>
 
 
 
-using std::cout;
-using std::endl;
+
 
 
 int main()
 {
-	bool exit = false;
-	int ch;
-
-	initscr();
-	raw();
-	keypad(stdscr, TRUE);
-	noecho();
-	do
-	{
-		printw("Type any character to see it in bold\n");
-		ch = getch();
-
-		if(ch == KEY_F(1)){
-			exit = true;
-
-			printw("F1 Key Pressed");
-		}
-		else
-		{
-			printw("the pressed key is ");
-			attron(A_BOLD);
-			printw("%c", ch);
-			attroff(A_BOLD);
-
-		}
-
-	} while (!exit);
 	
 	
 
 
 
-
-
-refresh();
-//getch();
-endwin();
 
 	
 }
